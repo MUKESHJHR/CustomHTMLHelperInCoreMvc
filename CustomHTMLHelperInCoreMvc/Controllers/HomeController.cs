89c4c1d9872ec73e2161e2598e386602180e0ec3
mukesh kumar <mukesh.1006jhr@outlook.com>
2023-10-22 04:53:24 +0530

    Add project files.

---
+++ CustomHTMLHelperInCoreMvc/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using CustomHTMLHelperInCoreMvc.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace CustomHTMLHelperInCoreMvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public ActionResult Index()
+        {
+            Employee employee = new Employee()
+            {
+                Id = 106724,
+                FullName = "Pranaya Rout",
+                Designation = "Manager",
+                Department = "IT",
+                Photo = "/Images/User.webp",
+                AlternateText = "Pranaya Rout Photo Not Available"
+            };
+            return View(employee);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ CustomHTMLHelperInCoreMvc/Models/Employee.cs
@@ -0,0 +1,12 @@
+﻿namespace CustomHTMLHelperInCoreMvc.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string? FullName { get; set; }
+        public string? Designation { get; set; }
+        public string? Department { get; set; }
+        public string? Photo { get; set; }
+        public string? AlternateText { get; set; }
+    }
+}
